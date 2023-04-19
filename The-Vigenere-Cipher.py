8bd3caf41667c9817580ad33da1d5a77a531534a
KaninWithRice <mishalnorona014@gmail.com>
2023-04-20 03:05:23 +0800

    Pseudocode

---
+++ The-Vigenere-Cipher.py
@@ -0,0 +1,11 @@
+#Mohammad Mishal S. Noroña | BSCPE 1-5 | PROBLEM 3 - The Vigenère Cipher
+# Import color module
+# add introduction
+# asks the user for the plaintext (all uppercase letters, no spaces)
+# define user's input
+# formula for encryption 
+# encrypt the message
+# decrypt the message
+# add loading time
+# add animation
+# print output
