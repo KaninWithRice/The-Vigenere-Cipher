74a3f0386c0af67b549693653cac37f4e7a72808
KaninWithRice <mishalnorona014@gmail.com>
2023-04-20 03:06:41 +0800

    Imported Colorama

--- The-Vigenere-Cipher.py
+++ The-Vigenere-Cipher.py
@@ -1,5 +1,8 @@
 #Mohammad Mishal S. Noroña | BSCPE 1-5 | PROBLEM 3 - The Vigenère Cipher
 # Import color module
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
 # add introduction
 # asks the user for the plaintext (all uppercase letters, no spaces)
 # define user's input
